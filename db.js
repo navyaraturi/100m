9953eb5eec1ce2e7b62abc1393c653776d983e9c
Navya Raturi <codewithnavya@proton.me>
2025-10-16 19:55:35 +0000

    add data schema

--- db.js
+++ db.js
@@ -1,4 +1,32 @@
 import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
-const ObjectId = Schema.Types.BigInt;
+const ObjectId = Schema.Types.ObjectId;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const snippetSchema = new Schema(
+  {
+    userId: {
+      type: ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+const Snippet = mongoose.model("Snippet", snippetSchema);
+
+export { User, Snippet };
